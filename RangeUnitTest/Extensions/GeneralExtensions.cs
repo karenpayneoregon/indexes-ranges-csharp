20e2392399ec73df90f64b724e64c3268ea293b2
Karen Payne <Karen.1.Payne@oregon.gov>
2021-06-06 08:36:01 -0700

    comments

--- RangeUnitTest/Extensions/GeneralExtensions.cs
+++ RangeUnitTest/Extensions/GeneralExtensions.cs
@@ -7,6 +7,17 @@ namespace RangeUnitTest.Extensions
 {
     public static class GeneralExtensions
     {
+        /// <summary>
+        /// Provides the ability to obtain a range for a list
+        /// </summary>
+        /// <typeparam name="T">List type</typeparam>
+        /// <param name="list">Actual list</param>
+        /// <param name="range"><see cref="Range"/></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// ranges are not supported for list which this method
+        /// fills this gap
+        /// </remarks>
         public static List<T> GetRange<T>(this List<T> list, Range range)
         {
             var (start, length) = range.GetOffsetAndLength(list.Count);
