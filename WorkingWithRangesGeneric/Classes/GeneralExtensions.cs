b28a86c46a41fe7908001fdda9785a00e4c09f4c
Karen Payne <Karen.1.Payne@oregon.gov>
2021-06-06 12:39:19 -0700

    Breakout code

---
+++ WorkingWithRangesGeneric/Classes/GeneralExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithRangesGeneric.Classes
+{
+    public static class GeneralExtensions
+    {
+        /// <summary>
+        /// Provides the ability to obtain a range for a list
+        /// </summary>
+        /// <typeparam name="T">List type</typeparam>
+        /// <param name="list">Actual list</param>
+        /// <param name="range"><see cref="Range"/></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// ranges are not supported for list which this method
+        /// fills this gap
+        /// </remarks>
+        public static List<T> GetRange<T>(this List<T> list, Range range)
+        {
+            var (start, length) = range.GetOffsetAndLength(list.Count);
+            return list.GetRange(start, length);
+        }
+    }
+}
---
+++ WorkingWithRangesGeneric/Classes/Helpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithRangesGeneric.Classes
+{
+    public class Helpers
+    {
+        /// <summary>
+        /// A method to learn how indices work against generic lists.
+        /// Not intended to use in an application.
+        /// </summary>
+        /// <typeparam name="T">type</typeparam>
+        /// <param name="list">list to work with</param>
+        /// <returns>list of <seealso cref="ElementContainer"/></returns>
+        public static List<ElementContainer<T>> RangeDetails<T>(List<T> list)
+        {
+            var elementsList = list.Select((element, index) => new ElementContainer<T>
+            {
+                Value = element,
+                StartIndex = new Index(index),
+                EndIndex = new Index(Enumerable.Range(0, list.Count).Reverse().ToList()[index], true)
+            }).ToList();
+
+            return elementsList;
+        }
+    }
+}
